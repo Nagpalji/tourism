0975e5751118a122652ab9cd575b5eb1ad0ceb6f
aditisharma6202 <111963350+aditisharma6202@users.noreply.github.com>
2023-04-16 21:35:38 +0530

    3rd page update

--- src/pages/Accessibility.js
+++ src/pages/Accessibility.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import Header from './header';
 import SiteNav from './sitenav';
-import Chart from "react-apexcharts";
+import golden2 from '../images/golden2.jpg';
+import golden1 from '../images/golden1.jpg'
+import golden3 from '../images/golden3.jpeg';
+import page3 from '../images/page3.jpg';
 import Barchart from './barchart';
 function Accessibility() {
     return ( <>
@@ -16,7 +19,18 @@ function Accessibility() {
                 <h6 className='p-1 pt-4'>2. Score on public transport facility near Golden Temple</h6>
                 <Barchart/>
             </div>
-            <div className='col-md-4 col-12'></div>
+            <div className='col-md-4 col-12'>
+                <div className='side-image-bar'>
+                    <div className='d-flex'>
+<img src={page3} alt='satellite image' height={'300'}/>
+<img src={golden1} alt='golden temple' height={'300'}/>
+                    </div>
+                    <div className='d-flex'>
+                        <img src={golden2} alt='golden temple' height={'400'}/>
+                        <img src={golden3} alt='golden temple' height={'400'}/>
+                    </div>
+                </div>
+            </div>
 
         </div>
     </div>
